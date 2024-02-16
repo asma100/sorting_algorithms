1f6860c3b85b4512e27e53a877cf0b0106ddd9cd
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-16 12:10:59 -0800

    bubble +selection sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * bubble_sort - type of sorting
+ * @array: input
+ * @size:size of input
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+int i;
+int j;
+int temp;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
+}
