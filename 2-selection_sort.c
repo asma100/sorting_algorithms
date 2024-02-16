704f20abc81b08475b2f86a6005d7502433989ff
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-16 13:45:28 -0800

    selection editing

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,37 +2,32 @@
 #include <stdlib.h>
 #include "sort.h"
 /**
- * selection_sort - type of sorting
- * @array: input
- * @size:size of input
- * Return: void
- */
+  * selection_sort - selection sort algorithm
+  * @array: array to sort
+  * @size: size of array
+  */
 void selection_sort(int *array, size_t size)
 {
-size_t i;
-size_t j;
-size_t temp;
-int min;
-size_t index;
-if (array == NULL)
-return;
-for (i = 0; i < size - 1; i++)
-{
-min = array[i];
-for (j = i + 1; j < size; j++)
-{
-if (array[j] < min)
-{
-min = array[j];
-index = j;
-}
-}
-if (i != index)
-{
-temp = array[i];
-array[i] = array[index];
-array[index] = temp;
-print_array(array, size);
-}
-}
+	size_t i, i2;
+	int min, tmp, idx;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		for (i2 = i + 1; i2 < size; i2++)
+		{
+			if (min > array[i2])
+			{
+				min = array[i2];
+				idx = i2;
+			}
+		}
+		if (min != array[i])
+		{
+			tmp = array[i];
+			array[i] = min;
+			array[idx] = tmp;
+			print_array(array, size);
+		}
+	}
 }
---
+++ 2-selection_sortt.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * selection_sort - type of sorting
+ * @array: input
+ * @size:size of input
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+size_t i;
+size_t j;
+size_t temp;
+int min;
+size_t index;
+if (array == NULL)
+return;
+for (i = 0; i < size - 1; i++)
+{
+min = array[i];
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < min)
+{
+min = array[j];
+index = j;
+}
+}
+if (i != index)
+{
+temp = array[i];
+array[i] = array[index];
+array[index] = temp;
+print_array(array, size);
+}
+}
+}
