d92ac32a5bd2bf23bea4a71d08e04611acbfb111
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-16 12:31:43 -0800

    some code fixing

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,12 +8,12 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-int i;
-int j;
+size_t i;
+size_t j;
 int temp;
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
+for (j = 0; j < size - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
