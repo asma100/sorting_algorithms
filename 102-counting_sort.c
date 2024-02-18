c9e855601ead3eb7a72860cf940f1bc53faeea03
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-18 08:15:02 -0800

    cou

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,43 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 /**
- * counting_sort - sorts an array of integers in ascending
- * order using the Counting sort algorithm
- * @array: pointer to array
- * @size: size of the array
- **/
+ * duplicate_array - type of sorting
+ * @original_array: input
+ * @size:size of input
+ * Return: copy of original array
+ */
+int *duplicate_array(int *original_array, int size)
+{
+int *duplicate_array = (int *)malloc(size * sizeof(int));
+int i;
+for (i = 0; i < size; i++)
+{
+duplicate_array[i] = original_array[i];
+}
+return (duplicate_array);
+free(duplicate_array);
+}
+/**
+ * counting_sort - type of sorting
+ * @array: input
+ * @size:size of input
+ * Return: void
+ */
 void counting_sort(int *array, size_t size)
 {
-	int n, j, *count_array, *aux;
-	size_t i;
-
-	if (!array || size < 2)
-		return;
-	n = array[0];
-	for (i = 0; i < size; i++)
-	{
-		if (array[i] > n)
-			n = array[i];
-	}
-	count_array = calloc((n + 1), sizeof(int));
-	for (i = 0; i < size; i++)
-	{
-		count_array[array[i]]++;
-	}
-	for (j = 1; j < n; j++)
-	{
-		count_array[j + 1] += count_array[j];
-	}
-	print_array(count_array, n + 1);
-	aux = malloc(sizeof(int) * size);
-	for (i = 0; i < size; i++)
-	{
-		count_array[array[i]]--;
-		aux[count_array[array[i]]] = array[i];
-	}
-	for (i = 0; i < size; i++)
-	{
-		array[i] = aux[i];
-	}
-	free(aux);
-	free(count_array);
+int x, y, j, k;
+size_t i, l;
+int max = array[0];
+int *coun_arr;
+int *cp_arr;
+for (i = 0; i < size; i++)
+{
+if (array[i] > max)
+max = array[i];
+}
+coun_arr = malloc((max + 1) * sizeof(int));
+for (j = 0; j < max + 1; j++)
+coun_arr[j] = 0;
+for (k = 0; k < max + 1; k++)
+{
+for (l = 0; l < size; l++)
+{
+if (k == array[l])
+{
+coun_arr[k] = coun_arr[k] + 1;
+}
+}
+}
+for (x = 0; x < max + 1; x++)
+{
+coun_arr[x + 1] += coun_arr[x];
+}
+print_array(coun_arr, max + 1);
+cp_arr = malloc(sizeof(int) * size);
+for (y = size - 1; y >= 0; y--)
+{
+coun_arr[array[y]]--;
+cp_arr[coun_arr[array[y]]] = array[y];
+}
+for (i = 0; i < size; i++)
+{
+array[i] = cp_arr[i];
+}
+free(cp_arr);
+free(coun_arr); 
 }
