b139b6a35cfb407d2f1d824689fd61fb7647fad6
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-18 06:30:50 -0800

    editing

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,68 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "sort.h"
-/**
- * duplicate_array - type of sorting
- * @original_array: input
- * @size:size of input
- * Return: copy of original array
- */
-int *duplicate_array(int *original_array, int size)
-{
-int *duplicate_array = (int *)malloc(size * sizeof(int));
-int i;
-for (i = 0; i < size; i++)
-{
-duplicate_array[i] = original_array[i];
-}
-return (duplicate_array);
-free(duplicate_array);
-
-
-}
-/**
- * counting_sort - type of sorting
- * @array: input
- * @size:size of input
- * Return: void
- */
 void counting_sort(int *array, size_t size)
 {
-int c, x, y, b, j, k;
-size_t i, l;
-int max = array[0];
-int *coun_arr;
-int *cp_arr;
-for (i = 0; i < size; i++)
-{
-if (array[i] > max)
-max = array[i];
-}
-coun_arr = malloc((max + 1) * sizeof(int));
-for (j = 0; j < max + 1; j++)
-coun_arr[j] = 0;
-for (k = 0; k < max + 1; k++)
-{
-for (l = 0; l < size; l++)
-{
-if (k == array[l])
-{
-coun_arr[k] = coun_arr[k] + 1;
-}
-}
-}
-for (x = 1; x < max + 1; x++)
-{
-coun_arr[x] = coun_arr[x] + coun_arr[x - 1];
-}
-print_array(coun_arr, max + 1);
-cp_arr = duplicate_array(array, size);
-for (y = size - 1; y >= 0; y--)
-{
-c = cp_arr[y];
-b = coun_arr[c];
-array[b - 1] = c;
-coun_arr[c]--;
-}
-free(coun_arr); 
+	int *count_arr, k;
+	size_t i, j, arr_size;
+
+	if (array == NULL || size <= 1)
+		return;
+	arr_size = array[0];
+	for (i = 0; array[i]; i++)
+	{
+		if (array[i] > (int)arr_size)
+			arr_size = array[i];
+	}
+
+	arr_size += 1;
+
+	count_arr = malloc(arr_size * sizeof(int *));
+	if (count_arr == NULL)
+		return;
+
+	for (i = 0; i < arr_size; i++)
+		count_arr[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count_arr[array[i]] += 1;
+
+	for (i = 0; i <= arr_size; i++)
+		count_arr[i] += count_arr[i - 1];
+
+	print_array(count_arr, arr_size);
+
+	for (i = 1, j = 0; i <= arr_size; i++)
+		if (count_arr[i] != count_arr[i - 1])
+		{
+			for (k = 0; k < count_arr[i] - count_arr[i - 1]; k++)
+				array[j++] = i;
+		}
+	free(count_arr);
 }
